77eabc48cfd1bf407ad990e538ef1786c7ab2f8e
5ec1cff <ewtqyqyewtqyqy@gmail.com>
2023-06-20 21:52:12 +0800

    Refactor MyLinkMovementMethod: fix multiple links, avoid recognizing white area as link

--- app/src/main/java/io/github/a13e300/ro_tieba/view/MyLinkMovementMethod.kt
+++ app/src/main/java/io/github/a13e300/ro_tieba/view/MyLinkMovementMethod.kt
@@ -25,51 +25,65 @@ object MyLinkMovementMethod : ScrollingMovementMethod() {
         }
     }
 
+    data class LastClick(
+        val span: URLSpan
+    )
+
     override fun onTouchEvent(widget: TextView, buffer: Spannable, event: MotionEvent): Boolean {
-        val x = event.x - widget.totalPaddingLeft + widget.scrollX
-        val y = event.y.toInt() - widget.totalPaddingTop + widget.scrollY
-        val layout = widget.layout
-        val line = layout.getLineForVertical(y)
-        val off = layout.getOffsetForHorizontal(line, x)
-        buffer.getSpans(line, off, URLSpan::class.java).firstOrNull()?.let { link ->
-            when (event.action) {
-                MotionEvent.ACTION_DOWN -> {
-                    if (link is MyURLSpan) link.pressed = true
-                    widget.invalidate()
-                    val checkForLongClick = CheckForLongClick(widget, event.x, event.y)
-                    widget.postDelayed(checkForLongClick, LONG_PRESS_THRESHOLD)
-                    widget.setTag(R.id.tag_last_click_time, checkForLongClick)
-                    var parent = widget.parent
-                    while (parent != null) {
-                        if (parent is ItemView) {
-                            parent.setSelectedData(SelectedLink(link.url))
-                            break
-                        }
-                        parent = parent.parent
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+                val x = event.x - widget.totalPaddingLeft + widget.scrollX
+                val y = event.y.toInt() - widget.totalPaddingTop + widget.scrollY
+                val layout = widget.layout
+                val line = layout.getLineForVertical(y)
+                if (x > layout.getLineWidth(line)) return false
+                val off = layout.getOffsetForHorizontal(line, x)
+                val span =
+                    buffer.getSpans(off, off, URLSpan::class.java).firstOrNull() ?: return false
+                if (span is MyURLSpan) span.pressed = true
+                widget.invalidate()
+                widget.setTag(R.id.tag_movement_method_last_click, LastClick(span))
+                val checkForLongClick = CheckForLongClick(widget, event.x, event.y)
+                widget.postDelayed(checkForLongClick, LONG_PRESS_THRESHOLD)
+                widget.setTag(R.id.tag_movement_method_longclick, checkForLongClick)
+                var parent = widget.parent
+                while (parent != null) {
+                    if (parent is ItemView) {
+                        parent.setSelectedData(SelectedLink(span.url))
+                        break
                     }
+                    parent = parent.parent
                 }
+                return true
+            }
 
-                MotionEvent.ACTION_UP -> {
-                    if (link is MyURLSpan) link.pressed = false
-                    widget.invalidate()
-                    if (event.eventTime - event.downTime < LONG_PRESS_THRESHOLD) {
-                        (widget.getTag(R.id.tag_last_click_time) as? CheckForLongClick)?.let {
-                            widget.removeCallbacks(it)
-                        }
-                        link.onClick(widget)
-                    }
-                }
-
-                MotionEvent.ACTION_CANCEL -> {
-                    if (link is MyURLSpan) link.pressed = false
-                    widget.invalidate()
-                    (widget.getTag(R.id.tag_last_click_time) as? CheckForLongClick)?.let {
+            MotionEvent.ACTION_UP -> {
+                val link = (widget.getTag(R.id.tag_movement_method_last_click) as? LastClick)?.span
+                    ?: return false
+                if (link is MyURLSpan) link.pressed = false
+                widget.invalidate()
+                if (event.eventTime - event.downTime < LONG_PRESS_THRESHOLD) {
+                    (widget.getTag(R.id.tag_movement_method_longclick) as? CheckForLongClick)?.let {
                         widget.removeCallbacks(it)
                     }
+                    link.onClick(widget)
                 }
+                widget.setTag(R.id.tag_movement_method_last_click, null)
+                return true
             }
-            return super.onTouchEvent(widget, buffer, event)
+
+            MotionEvent.ACTION_CANCEL -> {
+                val link = (widget.getTag(R.id.tag_movement_method_last_click) as? LastClick)?.span
+                    ?: return false
+                if (link is MyURLSpan) link.pressed = false
+                widget.invalidate()
+                (widget.getTag(R.id.tag_movement_method_longclick) as? CheckForLongClick)?.let {
+                    widget.removeCallbacks(it)
+                }
+                widget.setTag(R.id.tag_movement_method_last_click, null)
+                return true
+            }
         }
-        return false // super.onTouchEvent(widget, buffer, event) // .also { Logger.d("handled=$it event=$event") }
+        return false
     }
 }
