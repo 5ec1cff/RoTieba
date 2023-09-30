adf7367edf925fe2572d442e53c83522b9d4921a
5ec1cff <ewtqyqyewtqyqy@gmail.com>
2023-09-30 23:25:39 +0800

    wip

--- app/src/main/java/io/github/a13e300/ro_tieba/ui/dashboard/DashboardViewModel.kt
+++ app/src/main/java/io/github/a13e300/ro_tieba/ui/dashboard/DashboardViewModel.kt
@@ -7,7 +7,7 @@ import androidx.lifecycle.ViewModel
 class DashboardViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is dashboard Fragment"
+        value = "WIP"
     }
     val text: LiveData<String> = _text
 }
