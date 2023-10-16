d11c08a4c002d27536e4ac5a59b0be91e36fdadb
5ec1cff <ewtqyqyewtqyqy@gmail.com>
2023-10-16 10:47:28 +0800

    fix clickable image has no ripple effect in night mode

--- app/src/main/java/io/github/a13e300/ro_tieba/view/MyImageFilterView.kt
+++ app/src/main/java/io/github/a13e300/ro_tieba/view/MyImageFilterView.kt
@@ -1,8 +1,8 @@
 package io.github.a13e300.ro_tieba.view
 
 import android.content.Context
-import android.graphics.drawable.ColorDrawable
-import android.graphics.drawable.LayerDrawable
+import android.graphics.Canvas
+import android.graphics.Paint
 import android.util.AttributeSet
 import androidx.constraintlayout.utils.widget.ImageFilterView
 import io.github.a13e300.ro_tieba.R
@@ -11,20 +11,31 @@ class MyImageFilterView : ImageFilterView {
     // w / h
     var imageRatio: Float = 0f
     var imageScale: Float = 0f
-    constructor(context: Context) : super(context)
+    private var needMask = false
+    private lateinit var mMaskPaint: Paint
+
+    constructor(context: Context) : super(context) {
+        init()
+    }
     constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {
+        init()
+    }
+
+    private fun init() {
         val ta = context.obtainStyledAttributes(intArrayOf(androidx.appcompat.R.attr.isLightTheme))
-        val isLight = ta.getBoolean(0, false)
-        ta.recycle()
-        if (!isLight) {
-            val fg = foreground
-            val layer =
-                LayerDrawable(arrayOf(ColorDrawable(context.getColor(R.color.image_shadow))))
-            if (fg != null) {
-                layer.addLayer(fg)
-            }
-            foreground = layer
+        needMask = !ta.getBoolean(0, false)
+        if (needMask) mMaskPaint = Paint().apply {
+            color = context.getColor(R.color.image_shadow)
+            style = Paint.Style.FILL
         }
+        ta.recycle()
+    }
+
+    override fun onDrawForeground(canvas: Canvas) {
+        if (needMask) {
+            canvas.drawRect(0f, 0f, width.toFloat(), height.toFloat(), mMaskPaint)
+        }
+        super.onDrawForeground(canvas)
     }
 
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
